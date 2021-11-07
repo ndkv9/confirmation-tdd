a82b3fe60c1f9f9de2803fb4acd1f61a890ec177
ndkv9 <vu.nguyen101@gmail.com>
2021-11-07 13:12:08 +0200

    add canceling handler

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -1,10 +1,10 @@
-const Confirmation = ({ children, handleConfirm, handelCancel }) => {
+const Confirmation = ({ children, handleConfirm, handleCancel }) => {
   return (
     <div role='dialog'>
       <h1>Confirmation</h1>
       {children}
       <button onClick={() => handleConfirm()}>OK</button>
-      <button onClick={() => handelCancel()}>Cancel</button>
+      <button onClick={() => handleCancel()}>Cancel</button>
     </div>
   )
 }
